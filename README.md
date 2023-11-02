c15e0e37616996d23616baa4556f10b86718bdf6
Ahmad Hassan <ahmadalink1@gmail.com>
2023-11-03 00:01:06 +0500

    component complete using CSS & JS

---
+++ README.md
@@ -0,0 +1,28 @@
+# Interactive Rating Component👨‍💻
+
+## Project Overview 🌟
+
+This is a front-end coding challenge completed for [Frontend Mentor](https://www.frontendmentor.io). The challenge was to build an interactive rating component and make it look as close to the provided design as possible.
+
+## Technologies Used 💻
+
+- HTML
+- CSS
+
+### The Challenge 📋
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Select and submit a number rating
+- See the "Thank you" card state after submitting a rating
+## Screenshots 📷
+
+| Mobile                                            | Desktop                                            |
+| ------------------------------------------------- | -------------------------------------------------- |
+| ![](./screenshort/mobile-preview.png)             | ![](./screenshort/desktop-preview.png)             |
+
+## Live Demo 🚀
+
+You can view the live demo of the project [here](https://interactive-rating-component-js10.netlify.app/).
---
+++ index.js
@@ -0,0 +1,49 @@
+const ratings = document.querySelectorAll('.rating__f-box li');
+const submitBtn = document.querySelector('.rating__submit__btn');
+const rateAgainBtn = document.querySelector('.thanks__rate__again__btn');
+const ratingSection = document.querySelector('.rating-container');
+const thanksSection = document.querySelector('.thanks-component');
+const ratePoint = document.querySelector('.rate'); 
+
+// Add a click event listener to each rating element
+ratings.forEach((rating) => {
+    rating.addEventListener('click', (e) => {
+        // Step 1: Clear any existing selected rating
+        removeColor();
+
+        // Step 2: Highlight the clicked rating with the 'orange' class
+        rating.classList.add('orange');
+
+        // Step 3: Update the displayed rating value
+        ratePoint.innerText = e.currentTarget.innerText;
+        console.log('Selected rating:', e.currentTarget.innerText);
+        console.log('Displayed rating:', ratePoint.innerText);
+    });
+});
+
+// Add a click event listener to the submit button
+submitBtn.addEventListener('click', () => {
+    // Check if a rating has been selected (a rating value exists)
+    if (ratePoint.innerText) {
+        // Step 4: Show the thanks section and hide the rating section
+        thanksSection.classList.remove('hidden');
+        ratingSection.classList.add('rate-hidden');
+    } 
+});
+
+// Add a click event listener to the "Rate Again" button
+rateAgainBtn.addEventListener('click', () => {
+    // Step 5: Hide the thanks section and show the rating section again
+    thanksSection.classList.add('hidden');
+    ratingSection.classList.remove('rate-hidden');
+
+    // Step 6: Clear any existing selected rating
+    removeColor();
+});
+
+// Function to clear the 'orange' class from all ratings
+function removeColor() {
+    ratings.forEach((rating) => {
+        rating.classList.remove('orange');
+    });
+}
